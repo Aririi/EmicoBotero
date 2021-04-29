cd55bfa689d90ae1ee15ed7f2630abd23d25a119
Ariri <37724510+Aririi@users.noreply.github.com>
2021-04-28 23:23:58 -0700

    Added choose, xkcd, and random commands. Trivia has also been pushed but disabled until fixed. (#4)

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`Logged in as ${client.user.tag}`);
+		console.log(`Logged in successfully as ${client.user.tag}`);
 	},
 };
